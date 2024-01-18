aa28095764bfd8434034eeff9e2a7158ec3b0147
Mellling <154119653+Mellling@users.noreply.github.com>
2024-01-18 16:47:53 +0900

    HashTable Test
    해시테이블 실습

---
+++ HashTableTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Collections.Specialized.BitVector32;
+
+namespace HashTableTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheatKey cheatKey = new CheatKey();
+
+            cheatKey.Run("ShowMeTheMoney");
+            cheatKey.Run("ThereIsNoCowLevel");
+            cheatKey.Run("Null");
+        }
+
+        // A+ 치트키 구현
+        public class CheatKey
+        {
+            Dictionary<string, Action> cheatDic;
+
+            public CheatKey() 
+            {
+                cheatDic = new Dictionary<string, Action>();
+                Action action;
+
+                cheatDic.Add("ShowMeTheMoney", action = ShowMeTheMoney);
+                cheatDic.Add("ThereIsNoCowLevel", action = ThereIsNoCowLevel);
+            }
+
+            public void Run(string cheatKey)
+            {
+                Action action;
+                // 조건문 없이 바로 탐색하여 치트키 발동
+                cheatDic.TryGetValue(cheatKey, out action);
+
+                action?.Invoke();
+            }
+            
+            public void ShowMeTheMoney()
+            {
+                Console.WriteLine("골드를 늘려주는 치트키 발동!");
+            }
+
+            public void ThereIsNoCowLevel()
+            {
+                Console.WriteLine("바로 승리합니다 치트키 발동!");
+            }
+        }
+    }
+}
