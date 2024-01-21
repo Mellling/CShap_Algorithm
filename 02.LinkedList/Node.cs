4c9a852ba0e71abff67f1527bf812b9b21cf368d
Mellling <154119653+Mellling@users.noreply.github.com>
2024-01-21 22:44:33 +0900

    Update Node.cs

--- 02.LinkedList/Node.cs
+++ 02.LinkedList/Node.cs
@@ -11,26 +11,40 @@ namespace _02.LinkedList
         /**************************************************************
          * 노드 (Node)
          * 
-         * 여저 자료구조에서 사용하는 기본단위
+         * 여러 자료구조에서 사용하는 기본단위
          * 데이터와 다른 노드를 참조하는 값을 가짐
-         * 다른 노드의 참조를 연결하는 방식을 따라 여러 자료구조를 구현 가능
+         * 다른 노드의 참조를 연결하는 방식에 따라 여러 자료구조를 구현 가능
          **************************************************************/
 
         // 노드 구현
         // 노드에 데이터와 다른 노드를 참조하는 값을 구성하여 구현
-        // 연결된 노드를 가지는
+        // 연결된 노드를 가지는 일정갯수의 변수 또는 배열을 통해 고정적인 연결구조를 가짐
+        // 연결구조가 일정한 연결리스트/트리 등에 사용
 
-        public class LinkedListNode<T>
+        public class BinaryTreeNode<T>
         {
             public T value;
 
-            public LinkedListNode<T> prev;
-            public LinkedListNode<T> next;
-
-            private T item;
+            public BinaryTreeNode<T> parent;
+            public BinaryTreeNode<T> left;
+            public BinaryTreeNode<T> right;
         }
 
-        // 연결된 노드를 가지는 List를 통해 유돟적인 연결구조 가짐
-        // 연결구조가 일정하지 않는 트리/ 그래프에서 사용
+        public class OctTreeNode<T>
+        {
+            public T value;
+
+            public OctTreeNode<T> parent;
+            public OctTreeNode<T>[] children = new OctTreeNode<T>[8];
+        }
+
+        // 연결된 노드를 가지는 List를 통해 유동적인 연결구조를 가짐
+        // 연결구조가 일정하지 않은 트리/그래프 등에 사용
+        public class AdjacentNode<T>
+        {
+            public T value;
+
+            public List<AdjacentNode<T>> connect = new List<AdjacentNode<T>>();
+        }
     }
 }
