180f86ad4fd5d4a71cc1c615963c4abc2fd29bf7
Mellling <154119653+Mellling@users.noreply.github.com>
2024-01-23 13:15:27 +0900

    Search&Graph Study
    탐색 알고리즘과 그래프 스터디

---
+++ 10.Searching/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _10.Searching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 순차탐색
+            int[] array = { 1, 3, 5, 7, 9, 8, 6, 4, 2, 0 };
+            int sequentialIndex = Search.SequentialSearch(array, 2);
+            Console.Write("순차탐색 배열 : ");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("순차탐색 결과 위치 : {0}", sequentialIndex);
+            Console.WriteLine();
+
+            // 이진탐색
+            Array.Sort(array);  // 이진탐색의 경우 우선 정렬이 필요
+            int binaryIndex = Search.BinarySearch(array, 2);
+            Console.Write("이진탐색 배열 : ");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("이진탐색 결과 위치 : {0}", binaryIndex);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 10.Searching/Search.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Searching
+{
+    internal class Search
+    {
+        // <순차 탐색>
+        // 자료구조에서 순차적으로 찾고자 하는 데이터를 탐색
+        // 시간복잡도 - O(N)
+        public static int SequentialSearch<T>(in IList<T> list, in T item)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (item.Equals(list[i]))
+                    return i;
+            }
+            return -1;
+        }
+
+        // <이진 탐색>
+        // 정렬이 되어있는 자료구조에서 2분할을 통해 데이터를 탐색
+        // 시간복잡도 - O(logN)
+        public static int BinarySearch<T>(in IList<T> list, in T item) where T : IComparable<T>
+        {
+            int low = 0;
+            int high = list.Count - 1;
+            while (low <= high)
+            {
+                int middle = (low + high) / 2;
+                int compare = list[middle].CompareTo(item);
+
+                if (compare < 0)
+                    low = middle + 1;
+                else if (compare > 0)
+                    high = middle - 1;
+                else
+                    return middle;
+            }
+
+            return -1;
+        }
+    }
+}
